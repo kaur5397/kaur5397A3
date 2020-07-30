76098ccab82f5678f7439948ccfa634678743dd8
kaur5397 <47006796+kaur5397@users.noreply.github.com>
2020-07-29 23:16:22 -0400

    Add files via upload

---
+++ assets/data/myCars.ts
@@ -0,0 +1,29 @@
+import {CarDetails} from '../../app/carDetails';
+
+export const MYCARS : CarDetails[] = [
+    {
+        carMake:'Audi', 
+        carModel:'AudiA4',
+        bodyType:'sedan',color:'black',
+         picture:'assets/images/audi.jpg'},
+
+    {
+        carMake:'BMW',
+         carModel:'BMWX5',
+         bodyType:'convertible',
+         color:'black', 
+         picture:'assets/images/bmw.jpg'},
+    {
+        carMake:'Ferrari',
+         carModel:'ferrari Roma',
+         bodyType:'coupe',
+         color:'black',
+           picture:'assets/images/ferrari.jpg'},
+    {
+        carMake:'Mercedes', 
+        carModel:'Mercedes-benz GLB',
+        bodyType:'SUV',
+        color:'black',
+         picture:'assets/images/mercedes.jpg'}
+
+];
